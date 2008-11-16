6eda5f4a0f9f00d452031251ecce2817e57f6a08
stepan <stepan@b4ced013-0551-4a99-a96c-c7f3f828aea7>
2008-11-16 21:49:09 +0000

    Prepared Gallery HTML Generation

--- src/GalGenLib/Outputter/GalleryHTMLOutputter.py
+++ src/GalGenLib/Outputter/GalleryHTMLOutputter.py
@@ -3,7 +3,7 @@ from Thumbnailer import Thumbnailer
 from lxml import etree
 
 class GalleryHTMLOutputter(NamedObjectHTMLOutputter):
-    __column_count = 5
+    __column_count = 3
 
     def __init__(self, index):
         NamedObjectHTMLOutputter.__init__(self, index)
@@ -19,9 +19,26 @@ class GalleryHTMLOutputter(NamedObjectHTMLOutputter):
                 column_count = 0
             column_count += 1
             td = etree.SubElement(tr, 'td')
-            td.set('pic_location', child.pic_location)
+#            td.set('pic_location', child.pic_location)
             thumb = child.thumbnail
             td.text = child.name
+            img = etree.SubElement(tr, 'img')
+            img.set('src', 'thumbs/Album_Kanada.jpg')
+            img.set('style', 'float:left; margin-left:2px')
+            img.set('alt', 'Kanada-Album')
+            img.set('class', 'thumb')
+            div = etree.SubElement(td, 'div')
+            div.set('align', 'left')
+            a = etree.SubElement(div, 'a')
+            a.set('href', 'kanada.html')
+            a.set('class', 'album')
+            etree.SubElement(a, 'br')
+            etree.SubElement(a, 'br')
+            a.text = 'Kanada'
+            etree.SubElement(a, 'br')
+            span = etree.SubElement(a, 'span')
+            span.set('style', 'font-size:11px')
+            span.text = '2007'
 
     def generateOutput(self):
         self.updateTitle()

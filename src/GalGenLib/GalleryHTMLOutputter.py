fac1457f44c87fd08e41acd6b7b9224514f1a0f4
stepan <stepan@b4ced013-0551-4a99-a96c-c7f3f828aea7>
2008-12-25 23:47:09 +0000

    1st steps for making gallery output work

--- src/GalGenLib/GalleryHTMLOutputter.py
+++ src/GalGenLib/GalleryHTMLOutputter.py
@@ -11,36 +11,38 @@ class GalleryHTMLOutputter(NamedObjectHTMLOutputter):
         NamedObjectHTMLOutputter.__init__(self, index)
 
     def __addIndexTable(self):
+        thumb_size = Thumbnailer.getInstance().gallery_thumb_size
+        thumb_dir = 'thumbs/%dx%d' % (thumb_size[0], thumb_size[1])
         content_element = self.getContentTag()
         table = etree.SubElement(content_element, 'table')
+        table.set('cellpadding', '0')
+        table.set('cellcpacing', '0')
         tr = None
         column_count = 0
+        row_count = 0
         for child in self.entity.children:
             if tr is None or column_count == self.__column_count:
                 tr = etree.SubElement(table, 'tr')
+                row_count += 1
                 column_count = 0
             column_count += 1
-            td = etree.SubElement(tr, 'td')
-#            td.set('pic_location', child.pic_location)
-            td.text = child.name
-            img = etree.SubElement(tr, 'img')
-            img.set('src', 'thumbs/Album_Kanada.jpg')
-            img.set('style', 'float:left; margin-left:2px')
-            img.set('alt', 'Kanada-Album')
+            if row_count % 2:
+                title_td = etree.SubElement(tr, 'td')
+                pic_td = etree.SubElement(tr, 'td')
+            else:
+                pic_td = etree.SubElement(tr, 'td')
+                title_td = etree.SubElement(tr, 'td')
+            pic_a = etree.SubElement(pic_td, 'a')
+            img = etree.SubElement(pic_a, 'img')
+            img.set('src', '%s/%s' % (thumb_dir, child.pic_file_name))
             img.set('class', 'thumb')
-            div = etree.SubElement(td, 'div')
-            div.set('align', 'left')
-            a = etree.SubElement(div, 'a')
-            a.set('href', 'kanada.html')
-            a.set('class', 'album')
-            etree.SubElement(a, 'br')
-            etree.SubElement(a, 'br')
-            a.text = 'Kanada'
-            etree.SubElement(a, 'br')
-            span = etree.SubElement(a, 'span')
-            span.set('style', 'font-size:11px')
-            span.text = '2007'
+            title_a = etree.SubElement(title_td, 'a')
+            title_a.set('href', '%s/%s' % (thumb_dir, child.pic_file_name))
+            title_a.text = child.title
+            if not title_a.text:
+                title_a.text = child.name
 
+            
     def __createSubDirs(self, gallery_dir):
         os.mkdir(os.path.join(gallery_dir, 'thumbs'))
         gallery_thumb_size = Thumbnailer.getInstance().gallery_thumb_size
